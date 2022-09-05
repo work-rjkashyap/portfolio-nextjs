df83a6d175f5a77eb8ff80244a10d02930be8fac
Rajeshwar Kashyap <work.rjkashyap05@gmail.com>
2022-09-05 23:46:27 +0530

    new entry

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import '../styles/globals.scss';
+import NextNProgress from 'nextjs-progressbar';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<NextNProgress options={{ easing: 'ease', speed: 500 }} />
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
